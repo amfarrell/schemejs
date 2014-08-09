bc26fc9e17e8d3c465adc040845f76198e62ac01
Andrew M. Farrell <amfarrell@mit.edu>
2014-08-09 09:16:48 -0400

    add builtin arithmetic

--- interpreter.js
+++ interpreter.js
@@ -5,9 +5,21 @@ Terp.prototype = {
     eval: function(expr, env) {
         if (this._is_literal(expr)) {
             return this._eval_literal(expr);
+        } else if (this._is_builtin(expr)){
+            return this._builtins[expr];
         } else {
             return "Invalid expression.";
         }
+    },
+    _builtins: {
+        '+': function (operand1, operand2) {return operand1 + operand2},
+        '-': function (operand1, operand2) {return operand1 - operand2},
+        '*': function (operand1, operand2) {return operand1 * operand2},
+        '/': function (operand1, operand2) {return operand1 / operand2},
+        '%': function (operand1, operand2) {return operand1 % operand2},
+    },
+    _is_builtin: function(expr, env) {
+        return (expr in this._builtins)
     },
     _is_literal: function(expr, env) {
         //is expr a number?
@@ -17,5 +29,19 @@ Terp.prototype = {
     _eval_literal: function(expr, env) {
         return (new Number(expr)).valueOf();
     },
+    /*
+    _is_definition: function(expr, env) {
+        return this._first(expr) === 'def';
+    },
+    _eval_definition: function(expr, env) {
+        
+    },
+    _is_variable: function(expr, env) {
+
+    },
+    _eval_variable: function(expr, env) {
+
+    },
+    */
 }
 exports.Interpreter = Terp;
--- spec/interpreter-spec.js
+++ spec/interpreter-spec.js
@@ -14,6 +14,16 @@ describe("evaluation", function() {
         var result = (new interpreter.Interpreter()).eval("2.3e-3");
         expect(result).toBe(0.0023);
     });
+    it("Should handle literal functions ", function (){
+        var result = (new interpreter.Interpreter()).eval("2.3e-3");
+        expect(result).toBe(0.0023);
+    });
+    it("Should handle builtin arithmetic", function (){
+        var plus = (new interpreter.Interpreter()).eval("+");
+        expect(plus(2,3)).toBe(5);
+        var mod = (new interpreter.Interpreter()).eval("%");
+        expect(mod(8,3)).toBe(2);
+    });
     it("Should error on any other expression", function (){
         var result = (new interpreter.Interpreter()).eval("( + 2 3)");
         expect(result).toBe("Invalid expression.");
